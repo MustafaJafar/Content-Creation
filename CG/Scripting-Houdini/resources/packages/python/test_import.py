51e111a4cea17a75d4a600a461fa9a0369303a9a
Mustafa-Zarkash <mustafataherzaky@outlook.com>
2023-08-19 16:11:58 +0300

    update examples

--- CG/Scripting-Houdini/resources/packages/python/test_import.py
+++ CG/Scripting-Houdini/resources/packages/python/test_import.py
@@ -1,2 +1,14 @@
+""" Test Import Module. 
+
+This module was made to test importing custom scripts.
+you can test in houdini as follows: 
+1. create "python" Node and add this code:
+
+from test_import import test
+test()
+
+"""
+
+
 def test () : 
     print("This is printed from python external library\n")
